753cd2cfb54b2f60212ed8699dd2f46580c75d6f
Topor Denis <topor_denis16@yahoo.com>
2022-06-22 13:04:28 +0300

    Configuration window frontend finished

--- ESClientsideWeb/SettingsPage/index.js
+++ ESClientsideWeb/SettingsPage/index.js
@@ -1,20 +1,22 @@
 var optionTemplates = {
-    checkbox: `<div class="option">
+    checkbox: `
     <div class="name">{OPTION_NAME}</div>
     <div class="input">
         <label class="switch">
-            <input class="input__checkbox" type="checkbox" {OPTION_VALUE}>
+            <input class="input__checkbox" type="checkbox" propertyName={PROPERTY_NAME} {OPTION_VALUE}>
             <span class="slider round"></span>
         </label>
     </div>
-</div>`,
-    textinput: `<div class="option">
+`,
+    textinput: `
 <div class="name">{OPTION_NAME}</div>
 <div class="input">
-    <input type="text" class="input__text" name="{OPTION_NAME}" value="{OPTION_VALUE}" placeholder="{OPTION_PLACEHOLDER}" >
+    <input type="text" class="input__text" name="{OPTION_NAME}" propertyName={PROPERTY_NAME} value="{OPTION_VALUE}" placeholder="{OPTION_PLACEHOLDER}" >
 </div>
-</div>`
+`
 }
+var currentPopulatedSettings = [];
+
 function getTemplateByType(type) {
     switch (type) {
         case 'bool':
@@ -28,21 +30,22 @@ function fillTemplatebyType(setting) {
     var template = getTemplateByType(setting.type);
 
     var tempTemplate = template.replace('{OPTION_NAME}', setting.name);
-    
+
     if (tempTemplate) {
         if (setting.type == 'bool')
             tempTemplate = tempTemplate.replace('{OPTION_VALUE}', setting.value ? 'checked' : '');
         else if (setting.type == 'text') {
             tempTemplate = tempTemplate.replace('{OPTION_VALUE}', setting.value);
 
-          
+
             if (setting.value_placeholder)
                 tempTemplate = tempTemplate.replace('{OPTION_PLACEHOLDER}', setting.value_placeholder);
             else
                 tempTemplate = tempTemplate.replace('{OPTION_PLACEHOLDER}', '');
         }
+        tempTemplate = tempTemplate.replace('{PROPERTY_NAME}', setting.propertyName);
     }
-    
+
     return tempTemplate;
 }
 function populateSettings(settings) {
@@ -57,9 +60,54 @@ function populateSettings(settings) {
         if (!template) return;
 
         let doc = document.createElement('div');
+        doc.className = 'option';
         console.log('created doc ' + template);
         doc.innerHTML = template;
         settingsParrent.appendChild(doc);
-
+        setting.ref = doc;
     });
+
+    currentPopulatedSettings = settings;
 }
+
+
+function cancelCommand() {
+
+}
+function saveCommand() {
+    currentPopulatedSettings.forEach(setting => {
+        var option = setting.ref;
+        if (!option) return;
+
+
+
+        if (setting.type == 'text')
+            setting.value = option.children[1].children[0].value;
+        else if (setting.type == 'bool') {
+            setting.value = option.children[1].children[0].children[0].checked;
+            console.log('checked ' + option.children[1].children[0].children[0].checked);
+        }
+        setting.ref = null;
+    });
+
+    var json = JSON.stringify(currentPopulatedSettings);
+
+    console.log('currentSettings ' + json);
+
+    window.chrome.webview.postMessage(json);
+}
+
+document.addEventListener('click', function (ev) {
+    if (ev.target && ev.target.classList.contains('input__checkbox') && ev.target.hasAttribute('propertyName')) {
+
+        var res = currentPopulatedSettings.find(p => p.propertyName == ev.target.getAttribute('propertyName'));
+
+        if (res) {
+           res.value = !res.value;
+          console.log('modified  res ' + res);
+        }
+
+        console.log('asds');
+    }
+});
+
